011fe69397e53c6bcaa016d02cf3a2c916b0a31f
devUgur <ugur44ist@gmail.com>
2024-04-29 19:28:03 +0200

    init

---
+++ src/components/sections/BaseSection.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="border p-9 font-bold text-2xl" id="about-view">
+    <h1>asdf</h1>
+  </div>
+</template>
+
+<style>
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseSection from "@/components/sections/BaseSection.vue"</script>
 
 <template>
-  <div id="about-view">
-    <h3>About View</h3>
+  <div class="border bg-slate-300 text-black p-9 font-bold text-2xl" id="about-view">
+    <h1>Home | About View</h1>
   </div>
+  <BaseSection />
 </template>
+
+<style>
+</style>
