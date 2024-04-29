266ed478bb62ca4160bc50962f4b236923c073ff
devUgur <ugur44ist@gmail.com>
2024-04-29 19:13:28 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 import { RouterLink, RouterView } from 'vue-router'
 import TopnavComponent from './components/TopnavComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
+import MenuButton from './components/nav/MenuButton.vue';
 </script>
 
 <template>
   <TopnavComponent></TopnavComponent>
   <RouterView />
   <FooterComponent></FooterComponent>
+  <div class="md:hidden lg:hidden">
+
+    <MenuButton />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/TopnavComponent.vue
+++ src/components/TopnavComponent.vue
@@ -1,20 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <div
     id="topnav-component"
     class="sticky top-0 container h-24 flex justify-between place-items-center"
   >
-    <div class="bg-blue-300">
+    <div class="bg-blue-300 ">
       <img class="w-full h-28 bg-red-300" src="@/assets/logo/logo.jpg" />
     </div>
-    <div id="links" class="flex gap-10">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About Us</RouterLink>
-      <RouterLink to="/services">Services</RouterLink>
-      <RouterLink to="/pricing">Pricing</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
-    </div>
+      <div id="links" class="flex gap-10 hidden sm:flex">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About Us</RouterLink>
+        <RouterLink to="/services">Services</RouterLink>
+        <RouterLink to="/pricing">Pricing</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </div>
   </div>
 </template>
 
